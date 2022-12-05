45da6619acda87d17135c9996f89251d6b11184c
Olle Johannesson <olle.johannesson@objectbay.com>
2022-12-05 17:34:22 +0100

    also cache profile pics 'by hand' in js

--- src/scripts/profilepic.js
+++ src/scripts/profilepic.js
@@ -2,12 +2,25 @@ import orig from '../assets/me.jpeg'
 import rapOpen from '../assets/me_rap_open.png'
 import rapClosed from '../assets/me_rap_closed.png'
 
+const jsCache = new Map()
+const jsCached = (fn, url) => {
+    if (jsCache.has(url)) {
+        fn(jsCache.get(url))
+    } else {
+        fetch(url)
+            .then(response => response.blob())
+            .then(URL.createObjectURL)
+            .then(ourl => jsCache.set(url, ourl))
+            .then(() => jsCached(fn, url))
+    }
+}
+
 export const getProfilePic = () => document.querySelector('#profile-picture')
 
 export const changeTo = (newImgUrl) => getProfilePic().src = newImgUrl
 
-export const openMouth = () => changeTo(rapOpen)
+export const openMouth = () => jsCached(changeTo, rapOpen)
 
-export const closeMouth = () => changeTo(rapClosed)
+export const closeMouth = () => jsCached(changeTo, rapClosed)
 
-export const restore = () => changeTo(orig)
+export const restore = () => jsCached(changeTo, orig)
